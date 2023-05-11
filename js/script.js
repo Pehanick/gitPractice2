80b061c46e64d9287163f964b74a7246e6627505
Pehanick <den.denill@mail.ru>
2023-05-11 10:36:52 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.main-slider').slick();
+    $('.sellers-slide').slick({
+        slidesToShow: 5
+    });
+    $('.hot-main').slick({
+        slidesToShow: 4
+    });
+});
